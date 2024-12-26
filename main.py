75b7aaa507400bf3de5de19d20a559a973db3d62
Jeroen-Penninck <jeroen.penninck@student.vives.be>
2024-12-27 00:52:34 +0100

    simplified code and made a proper Class

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-
+import os
+from options.add_event import add_event
 
 print("\n *************************************\n | Super Simple Commandline Calendar |\n *************************************\n")
 
-class Menu:
-	valid = False
-	valid_options = [1,2,3,4]
-	while valid == False:
-		print("1 Add Event")
-		print("2 View Calendar")
-		print("3 Export Calendar")
-		print("4 Exit Application")
+class Calendar:
+	def __init__(self):
+		pass
 
-		option = int(input())
-		for v in valid_options:
-			if v == option:
+	def menu(self):
+		valid = False
+		while valid == False:
+			print("1 Add Event")
+			print("2 View Calendar")
+			print("3 Export Calendar")
+			print("4 Exit Application")
+
+			option = int(input("\n"))
+			
+			if option == 1:
 				valid = True
+				add_event()
+
+if __name__ == "__main__":
+	Calendar().menu()
