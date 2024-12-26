c1f3e4cce856be3911cf8ee36bf89fbe4777bff6
Jeroen-Penninck <jeroen.penninck@student.vives.be>
2024-12-27 00:06:02 +0100

    command line prompt test

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 
 
 print("\n *************************************\n | Super Simple Commandline Calendar |\n *************************************\n")
+
+class Menu:
+	valid = False
+	valid_options = [1,2,3,4]
+	while valid == False:
+		print("1 Add Event")
+		print("2 View Calendar")
+		print("3 Export Calendar")
+		print("4 Exit Application")
+
+		option = int(input())
+		for v in valid_options:
+			if v == option:
+				valid = True
