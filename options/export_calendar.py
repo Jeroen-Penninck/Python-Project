a8756f76f324e2e20448fe88b8bb487dde207181
Jeroen-Penninck <jeroen.penninck@student.vives.be>
2024-12-29 16:34:03 +0100

    xlsx works properly

--- options/export_calendar.py
+++ options/export_calendar.py
@@ -1,34 +1,48 @@
 import csv
+import pandas as pd
 from options.view_calendar import view_calendar
 
 def dialogue_file_extension():
 	print("\nChoose file extension:\n1 Excel\n2 CSV\n")
 	extension = input()
 	if extension == "1":
-		extension = ".xlsb"
+		extension = ".xlsx"
 	elif extension == "2":
 		extension = ".csv"
 	else:
-		dialogue_file_extension()
+		extension = dialogue_file_extension()
 	return extension
 
 csv_data = []
 
-def export_csv(data):
-	file_extension = dialogue_file_extension()
-
-	filename = input("Filename: ")
-	if filename[-4:] == ".csv" or filename[-5:] == ".xlsx":
-		filename = "exports/" + filename
-	else:
-		filename = "exports/" + filename + file_extension
-
+def export_csv(filename, data):
 	with open(filename, mode="w", newline="", encoding="utf8") as file:
 		writer = csv.writer(file)
 		writer.writerow(["Date and time", "Title", "Location"])
 		for row in data:
 			writer.writerow(row)
 
+def export_xlsx(filename, data):
+	df = pd.DataFrame(data, columns=["Date and time", "Title", "Location"])
+	df.to_excel(filename, index=False)
+
+def export_handler(data):
+	file_extension = dialogue_file_extension()
+
+	filename = input("Filename: ")
+	if filename[-4:] == ".csv":
+		filename = "exports/" + filename
+		export_csv(filename, data)
+	elif filename[-5:] == ".xlsx":
+		filename = "exports/" + filename
+		export_xlsx(filename, data)
+	else:
+		filename = "exports/" + filename + file_extension
+		if file_extension == ".csv":
+			export_csv(filename, data)
+		else:
+			export_xlsx(filename, data)
+
 def export_calendar(connection):
 	results = view_calendar(connection)
-	export_csv(results)
+	export_handler(results)
