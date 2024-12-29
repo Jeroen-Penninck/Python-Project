9c4c20ebecddf351ca5f464c2b78fdc896425884
Jeroen-Penninck <jeroen.penninck@student.vives.be>
2024-12-29 14:05:08 +0100

    attempted file extension prompt (xlsx requires different formatting)

--- options/export_calendar.py
+++ options/export_calendar.py
@@ -1,11 +1,24 @@
 import csv
 from options.view_calendar import view_calendar
 
+def dialogue_file_extension():
+	print("\nChoose file extension:\n1 Excel\n2 CSV\n")
+	extension = input()
+	if extension == "1":
+		extension = ".xlsx"
+	elif extension == "2":
+		extension = ".csv"
+	else:
+		dialogue_file_extension()
+	return extension
+
 csv_data = []
 
 def export_csv(data):
+	file_extension = dialogue_file_extension()
+
 	filename = input("Filename: ")
-	filename = "csv/" + filename + ".csv"
+	filename = "exports/" + filename + file_extension
 	with open(filename, mode="w", newline="", encoding="utf8") as file:
 		writer = csv.writer(file)
 		writer.writerow(["Date and time", "Title", "Location"])
