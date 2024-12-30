a993ea90a02c069066e457f3f136ff5001c2dc8f
Jeroen-Penninck <jeroen.penninck@student.vives.be>
2024-12-30 17:51:55 +0100

    made the fields date_and_time, title required

--- options/events/add_event.py
+++ options/events/add_event.py
@@ -1,8 +1,12 @@
 import sqlite3
 
 def add_event(connection):
-	date_and_time = input("Date and time: | YYYY-MM-DD hh:mm:ss | ")
-	title = input("Title: ")
+	date_and_time = ""
+	while date_and_time == "":
+		date_and_time = input("Date and time: | YYYY-MM-DD hh:mm:ss | ")
+	title = ""
+	while title == "":
+		title = input("Title: ")
 	location = input("Location: ")
 
 	cursor = connection.cursor()
